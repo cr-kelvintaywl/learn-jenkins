a3e3a2f5b0d6e402d8858856cdee91cb8c223b7c
Kelvin Tay <kelvin.tay@cybereason.com>
2024-09-13 15:38:00 +0900

    Revert "test: mv properties out of node declaration"
    This reverts commit f7feec5c4a351170dfc08129c215ee0730d7fd44.
    
    Not possible to run `checkout` outside the context of a node definition.

--- separate_params/main.groovy
+++ separate_params/main.groovy
@@ -1,13 +1,13 @@
-checkout scm
-// NOTE: `load` is a Jenkinsfile-only construct;
-// This is not a Groovy-specific function
-def params = load pwd() + '/separate_params/params.groovy'
-
-properties([
-    parameters(params.all())
-])
-
 node {
+    checkout scm
+    // NOTE: `load` is a Jenkinsfile-only construct;
+    // This is not a Groovy-specific function
+    def params = load pwd() + '/separate_params/params.groovy'
+
+    properties([
+        parameters(params.all())
+    ])
+
     stage('verify') {
         sh "echo foobar value is ${params.foobar}"
         sh "echo fizzbuzz value is ${params.fizzbuzz}"
