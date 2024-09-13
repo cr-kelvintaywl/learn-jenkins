9e9e31011f084b89873ad87bc35ec36c651b4434
Kelvin Tay <kelvin.tay@cybereason.com>
2024-09-13 10:32:29 +0900

    fix: try use def to declare vars

--- separate_params/params.groovy
+++ separate_params/params.groovy
@@ -1,10 +1,10 @@
-foobar = string(
+def foobar = string(
     name: 'foobar',
     defaultValue: 'FOOBAR',
     description: 'param1',
 )
 
-fizzbuzz = string(
+def fizzbuzz = string(
     name: 'fizzbuzz',
     defaultValue: 'FIZZBUZZ',
     description: 'param2',
