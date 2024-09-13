356b781c75b7aab603c8f3afca5f4e5590361c52
Kelvin Tay <kelvin.tay@cybereason.com>
2024-09-13 10:34:27 +0900

    refactor: simplify; return array immediately

--- separate_params/params.groovy
+++ separate_params/params.groovy
@@ -1,16 +1,12 @@
-def foobar = string(
-    name: 'foobar',
-    defaultValue: 'FOOBAR',
-    description: 'param1',
-)
-
-def fizzbuzz = string(
-    name: 'fizzbuzz',
-    defaultValue: 'FIZZBUZZ',
-    description: 'param2',
-)
-
 return [
-    foobar,
-    fizzbuzz,
+    string(
+        name: 'foobar',
+        defaultValue: 'FOOBAR',
+        description: 'param1',
+    ),
+    string(
+        name: 'fizzbuzz',
+        defaultValue: 'FIZZBUZZ',
+        description: 'param2',
+    )
 ]
