7abb286cdc9d5ecceeb1bb499754e765662d1c7a
Kelvin Tay <kelvin.tay@cybereason.com>
2024-10-10 09:15:33 +0900

    feat: check Pythno env

---
+++ check_python_env/main.groovy
@@ -0,0 +1,35 @@
+node {
+    checkout scm
+    properties([
+        parameters([
+            string(
+                name: 'node',
+                description: 'which node to run Docker commands on',
+            ),
+            string(
+                name: 'image',
+                description: 'docker image to pull',
+            ),
+            booleanParam(
+                name: 'reload_parameters',
+                description: 'Reload job parameters from git and exit.',
+                defaultValue: true,
+            ),
+        ])
+    ])
+}
+
+if (params.reload_parameters) {
+    currentBuild.result = 'SUCCESS'
+    currentBuild.displayName = 'Load Params'
+    // exit early
+    return
+}
+
+node(params.node) {
+    stage('Python env inspect') {
+        sh 'which python3'
+        sh 'python --version'
+        sh 'pip3 list'
+    }
+}
