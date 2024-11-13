1107c99b61449ad15cd2e912a68127360f949e31
Kelvin Tay <kelvin.tay@cybereason.com>
2024-11-13 15:18:55 +0900

    chore: add investigation for env vars

---
+++ check_artifacts/main.groovy
@@ -0,0 +1,33 @@
+node {
+    checkout scm
+    properties([
+        parameters([
+            string(
+                name: 'node',
+                description: 'which node to run Python commands on',
+                defaultValue: 'master',
+            ),
+            booleanParam(
+                name: 'reload_parameters',
+                description: 'Reload job parameters from git and exit.',
+                defaultValue: true,
+            ),
+        ])
+    ])
+}
+
+if (params.reload_parameters) {
+    currentBuild.result = 'SUCCESS'
+    currentBuild.displayName = 'Load Params'
+    // exit early
+    return
+}
+
+node(params.node) {
+    stage('Env inspect') {
+        sh 'echo $JENKINS_HOME'
+        sh 'echo $WORKSPACE'
+        sh 'echo $BUILD_NUMBER'
+        sh 'echo $BUILD_URL'
+    }
+}
