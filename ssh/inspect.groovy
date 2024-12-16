fe2f19168008a6f08921d5d0c5c92e3edd68ab68
Kelvin Tay <kelvin.tay@cybereason.com>
2024-12-16 11:21:36 +0800

    inspect key

--- ssh/inspect.groovy
+++ ssh/inspect.groovy
@@ -7,24 +7,8 @@ node {
                 description: 'which node to run Docker commands on',
             ),
             string(
-                name: 'keyholder_node',
-                description: 'which node to copy key from',
-            ),
-            string(
-                name: 'ip',
-                description: 'ip or address to telnet',
-            ),
-            string(
-              name: 'user',
-              description: 'user to log in as'
-            ),
-            string(
               name: 'keypath',
               description: 'path to SSH key in keyholder_node to copy from'
-            ),
-            string(
-              name: 'cmd',
-              description: 'command to run remotely'
             ),
             booleanParam(
                 name: 'reload_parameters',
@@ -42,23 +26,10 @@ if (params.reload_parameters) {
     return
 }
 
-node(params.keyholder_node) {
-    cleanWs()
-    stage('Copy SSH Key from node') {
-        sh "cp ${params.keypath} the_key"
-        stash(
-            name: 'ssh_key',
-            includes: 'the_key',
-        )
-    }
-}
-
 node(params.node) {
-    // clean workspace after pipeline, so we remove the copied SSH key
     cleanWs()
-
-    stage('Inspect') {
-        unstash 'ssh_key'
-        sh "ssh -i ./the_key -o StrictHostKeyChecking=no ${params.user}@${params.ip} ${params.cmd}"
+    stage('Inspect SSH Key from node') {
+        sh "head -c 50 ${params.keypath}"
+        sh "md5 ${params.keypath} || echo 'no md5'"
     }
 }
